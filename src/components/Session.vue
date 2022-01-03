4d8c10e0d993b633280d2a334bd43a5b15a4152f
rash <sebastian.gepperth@ax-semantics.com>
2022-01-03 22:13:32 +0100

    use bunt-icon-button and material icon for fav starring

--- src/components/Session.vue
+++ src/components/Session.vue
@@ -12,15 +12,9 @@ a.c-linear-schedule-session(:class="{faved}", :style="style", :href="link", @cli
 		.bottom-info
 			.track(v-if="session.track") {{ getLocalizedString(session.track.name) }}
 			.room(v-if="showRoom && session.room") {{ getLocalizedString(session.room.name) }}
-	.fav-container(:class="faved ? ['faved'] : []")
-		svg.star(
-			@click.prevent="faved ? $emit('unfav', session.id) : $emit('fav', session.id)",
-			viewBox="0 0 24 24"
-		)
-			polygon(
-				:style="faved ? {fill: '#FFA000', stroke: '#FFA000'} : {stroke: '#FFA000', fill: 'white', strokeWidth: 1.5}"
-				points="14.43,10 12,2 9.57,10 2,10 8.18,14.41 5.83,22 12,17.31 18.18,22 15.83,14.41 22,10"
-			)
+	bunt-icon-button.btn-fav-container(@click.prevent.stop="faved ? $emit('unfav', session.id) : $emit('fav', session.id)")
+		svg.star(viewBox="0 0 24 24")
+			path(d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z")
 
 </template>
 <script>
@@ -185,23 +179,30 @@ export default {
 				text-align: right
 				color: $clr-secondary-text-light
 				ellipsis()
-	.fav-container
+	.btn-fav-container
 		display: none
 		position: absolute
-		width: 16px
-		height: 16px
 		top: 2px
-		right: 4px
-		&.faved
-			display: block
+		right: 2px
+		icon-button-style(style: clear)
+		svg path
+			fill: none
+			stroke: $clr-primary-text-light
+			stroke-width: 1px
+			vector-effect: non-scaling-stroke
+	&.faved
+		.btn-fav-container
+			display: inline-flex
+			svg path
+				fill: $clr-primary-text-light
 	&:hover
 		.info
 			border: 1px solid var(--track-color)
 			border-left: none
 			.title
 				color: var(--pretalx-clr-primary)
-		.fav-container
-			display: block
+		.btn-fav-container
+			display: inline-flex
 	// +below('m')
 	// 	min-width: 0
 </style>
