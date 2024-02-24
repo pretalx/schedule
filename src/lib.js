afd79402bcafd2cda62989ceb07b6eb747073e5d
rash <rash@rash.codes>
2024-02-24 20:47:02 +0100

    upgrade to vite and vue3

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
-import LinearSchedule from 'components/LinearSchedule'
-import GridSchedule from 'components/GridSchedule'
-import Session from 'components/Session'
+import LinearSchedule from '~/components/LinearSchedule'
+import GridSchedule from '~/components/GridSchedule'
+import Session from '~/components/Session'
 
 export {
 	LinearSchedule,
